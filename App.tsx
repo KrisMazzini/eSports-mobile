3c547e90d710b76e698750a094750cbf4f0c1231
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-09-14 00:10:27 -0300

    add Button test component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+type ButtonProps = {
+  title: string;
+}
+
+function Button(props:ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Hello React Native!</Text>
+      <Button title='Send 1' />
+      <Button title='Send 2' />
+      <Button title='Send 3' />
       <StatusBar style="auto" />
     </View>
   );
